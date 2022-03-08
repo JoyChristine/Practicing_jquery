900f4a105433bed7ca6aaee16a811ebe1fdf2be3
Joy Christine <joychristin2@gmail.com>
2022-03-08 12:28:20 +0300

    make image visible on click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+    $("p").click(function() {
+      $("img").show();
+    });
+});
+
+
 // jQuery('h1').click(function(){
 //     alert("This is a heading tag");
 //     alert("I told you, THIS IS A HEADER!");
@@ -12,16 +19,16 @@
 // });
 
 
-$(document).ready(function() {
-    $("h1").click(function() {
-      alert("This is a header.");
-    });
+// $(document).ready(function() {
+//     $("h1").click(function() {
+//       alert("This is a header.");
+//     });
   
-    $("p").click(function() {
-      alert("This is a paragraph.");
-    });
+//     $("p").click(function() {
+//       alert("This is a paragraph.");
+//     });
   
-    $("img").click(function() {
-      alert("This is an image.");
-    });
-});
+//     $("img").click(function() {
+//       alert("This is an image.");
+//     });
+// });
